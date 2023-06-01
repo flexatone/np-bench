3d2ae37a093907544e23dc5c368729d271dee331
Christopher Ariza <ariza@flexatone.com>
2023-06-01 08:56:03 -0700

    updated footer

--- doc/bnpy-scipy-2023/global-bottom.vue
+++ doc/bnpy-scipy-2023/global-bottom.vue
@@ -3,10 +3,9 @@
     <footer
     v-if="$slidev.nav.currentLayout !== 'cover'"
     >
-        <div class="absolute left-10px bottom-0px">
-<img src="/ra-logo-500x500.svg" class="h-20" />
+        <div class="absolute left-20px bottom-20px">
+<img src="/RA-Logo-White-TM.svg" class="h-8 opacity-20" />
         </div>
-
 
         <div class="absolute right-30px bottom-10px">
 Static Frame
