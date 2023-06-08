580c08c2e233df191c175d113d5b7c77961d0e6d
Christopher Ariza <ariza@flexatone.com>
2023-06-07 19:14:45 -0700

    added SF logo

--- doc/bnpy-scipy-2023/global-bottom.vue
+++ doc/bnpy-scipy-2023/global-bottom.vue
@@ -4,11 +4,11 @@
     v-if="$slidev.nav.currentLayout !== 'cover'"
     >
         <div class="absolute left-20px bottom-20px">
-<img src="/RA-Logo-White-TM.svg" class="h-8 opacity-20" />
+        <img src="/RA-Logo-White-TM.svg" class="h-8 opacity-26" />
         </div>
 
-        <div class="absolute right-30px bottom-10px">
-Static Frame
+        <div class="absolute right-36px bottom-16px">
+        <img src="/sf-logo-horiz.svg" class="h-9 opacity-26" />
         </div>
 
         <div class="absolute right-10px bottom-3px">
